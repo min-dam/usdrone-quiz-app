952972259e62470514a64db64dd67dce514a25fb
min-dam <brain.dr8866@gmail.com>
2025-04-02 11:05:09 +0900

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import DroneQuiz from "./App"; // ✅ 우리가 만든 컴포넌트 이름
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <DroneQuiz />  {/* ✅ 퀴즈 앱 출력 */}
   </React.StrictMode>
 );
