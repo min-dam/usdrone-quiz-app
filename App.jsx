b3266220257505f342697c9f8bb009637f7ade6c
min-dam <brain.dr8866@gmail.com>
2025-04-02 10:27:29 +0900

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-4 text-center">
+      <h1 className="text-2xl font-bold">US Drone Quiz App</h1>
+      <p>퀴즈 기능은 개발 중입니다 ✈️</p>
+    </div>
+  );
+}
+
+export default App;
